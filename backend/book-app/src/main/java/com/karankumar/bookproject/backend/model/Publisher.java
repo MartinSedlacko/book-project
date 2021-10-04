3e95fc06269cd8d9ca2356e73e6251f803f4bf7e
Martin Sedlacko <martin.sedlacko@aardwark.com>
2021-10-04 10:51:08 +0200

    create book-app module and email-service module

---
+++ backend/book-app/src/main/java/com/karankumar/bookproject/backend/model/Publisher.java
@@ -0,0 +1,64 @@
+/*
+    The book project lets a user keep track of different books they would like to read, are currently
+    reading, have read or did not finish.
+    Copyright (C) 2020  Karan Kumar
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    GNU General Public License as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+    PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.karankumar.bookproject.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "publisher",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "publisher_name_unique", columnNames = "name")
+        }
+)
+@Data
+@JsonIgnoreProperties(value = {"id", "books"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "books")
+@NamedEntityGraph(name = "Publisher.books",
+        attributeNodes = @NamedAttributeNode("books")
+)
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "publishers")
+    private Set<Book> books = new HashSet<>();
+
+    public Publisher(@NotBlank String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ backend/book-app/src/main/java/com/karankumar/bookproject/backend/model/account/User.java
@@ -0,0 +1,81 @@
+/*
+    The book project lets a user keep track of different books they would like to read, are currently
+    reading, have read or did not finish.
+    Copyright (C) 2020  Karan Kumar
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    GNU General Public License as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+    PURPOSE.  See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program.
+    If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.karankumar.bookproject.backend.model.account;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.karankumar.bookproject.backend.constraints.PasswordStrength;
+import com.karankumar.bookproject.backend.constraints.PasswordStrengthCheck;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents a single User
+ */
+@Entity
+@Builder
+@Data
+@JsonIgnoreProperties(value = {"id"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "uidgenerator")
+    @SequenceGenerator(name = "uidgenerator", sequenceName = "uid_sequence")
+    @Getter
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    // For the RegExp see https://owasp.org/www-community/OWASP_Validation_Regex_Repository
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+            flags = Pattern.Flag.CASE_INSENSITIVE,
+            message = "The email must conform to OWASP Validation Regex for email address"
+    )
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @PasswordStrengthCheck(PasswordStrength.STRONG)
+    private String password;
+
+    @NotNull
+    private boolean active;
+
+    // Fetch type can be eager as there are not many roles
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "user_role_user_id_fk")
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "user_role_role_id_fk")
+            )
+    )
+    private Set<Role> roles = new HashSet<>();
+}
