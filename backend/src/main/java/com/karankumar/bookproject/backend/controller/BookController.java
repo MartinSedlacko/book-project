35aa0ee6709f70e69b97ff494e55d74f7d5f2ac4
Martin Sedlacko <sedlackomartin@gmail.com>
2021-11-06 18:11:36 +0100

    BookControllerTest

--- backend/src/main/java/com/karankumar/bookproject/backend/controller/BookController.java
+++ backend/src/main/java/com/karankumar/bookproject/backend/controller/BookController.java
@@ -14,33 +14,33 @@
 
 package com.karankumar.bookproject.backend.controller;
 
-import com.karankumar.bookproject.backend.dto.BookPatchDto;
-import org.modelmapper.Converter;
-import org.modelmapper.AbstractConverter;
-import org.modelmapper.ModelMapper;
 import com.karankumar.bookproject.backend.dto.BookDto;
+import com.karankumar.bookproject.backend.dto.BookPatchDto;
 import com.karankumar.bookproject.backend.model.Book;
-import com.karankumar.bookproject.backend.model.BookGenre;
 import com.karankumar.bookproject.backend.model.BookFormat;
+import com.karankumar.bookproject.backend.model.BookGenre;
 import com.karankumar.bookproject.backend.model.PredefinedShelf;
 import com.karankumar.bookproject.backend.service.BookService;
 import com.karankumar.bookproject.backend.service.PredefinedShelfService;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -138,7 +138,7 @@ public class BookController {
     return bookService.updateBook(bookToUpdate, bookPatchDto);
   }
 
-  private Book convertToBook(BookDto bookDto) {
+  protected Book convertToBook(BookDto bookDto) {
     return modelMapper.map(bookDto, Book.class);
   }
 
--- backend/src/test/java/com/karankumar/bookproject/backend/controller/BookControllerTest.java
+++ backend/src/test/java/com/karankumar/bookproject/backend/controller/BookControllerTest.java
@@ -14,8 +14,10 @@
 
 package com.karankumar.bookproject.backend.controller;
 
+import com.karankumar.bookproject.backend.dto.BookDto;
+import com.karankumar.bookproject.backend.dto.BookPatchDto;
 import com.karankumar.bookproject.backend.model.Book;
-import com.karankumar.bookproject.backend.model.Shelf;
+import com.karankumar.bookproject.backend.model.account.User;
 import com.karankumar.bookproject.backend.service.BookService;
 import com.karankumar.bookproject.backend.service.PredefinedShelfService;
 import org.junit.jupiter.api.Test;
@@ -24,23 +26,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class BookControllerTest {
     private final BookController bookController;
     private final BookService mockedBookService;
+    private final PredefinedShelfService mockedPredefinedShelfService;
+
+    private final User fakeUser;
+    private static final String BOOK_NOT_FOUND_ERROR_MESSAGE = "Could not find book with ID %d";
 
     BookControllerTest() {
         mockedBookService = mock(BookService.class);
-        PredefinedShelfService mockedPredefinedShelfService = mock(PredefinedShelfService.class);
+        mockedPredefinedShelfService = mock(PredefinedShelfService.class);
         ModelMapper mockedModelMapper = mock(ModelMapper.class);
+        fakeUser = new User(1L, "fakeUser@karankumar.com", "fakePass",
+                true, new HashSet<>());
         bookController = new BookController(
                 mockedBookService,
                 mockedPredefinedShelfService,
@@ -108,5 +124,102 @@ class BookControllerTest {
 
 //        assertThatExceptionOfType(BookNotFoundException.class)
 //                .isThrownBy(bookController.delete(1L));
+    }
+
+    @Test
+    void givenBookDto_WhenAddBook_ThenReturnBook(){
+        BookDto bookDto = new BookDto();
+        bookDto.setTitle("Fake book");
+        Book book = new Book();
+        book.setTitle("Fake book");
+
+        when(bookController.convertToBook(bookDto)).thenReturn(book);
+        when(mockedBookService.save(book)).thenReturn(Optional.of(book));
+
+        Optional<Book> result = bookController.addBook(bookDto);
+
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertTrue(result.isPresent()),
+                () -> assertEquals(result.get().getTitle(),book.getTitle())
+        );
+
+        verify(mockedBookService, times(1)).save(book);
+    }
+
+    @Test
+    void givenBookIdAndBookPatchDto_WhenUpdate_ThenReturnUpdatedBook(){
+        Long id = 1L;
+        Book bookToUpdate = new Book();
+        bookToUpdate.setTitle("Fake bookToUpdate");
+        Book updatedBook = new Book();
+        updatedBook.setTitle("Fake updatedBook");
+        BookPatchDto bookPatchDto = new BookPatchDto();
+        bookPatchDto.setTitle("Fake updatedBook");
+
+        when(mockedBookService.findById(id)).thenReturn(Optional.of(bookToUpdate));
+        when(mockedBookService.updateBook(bookToUpdate, bookPatchDto)).thenReturn(updatedBook);
+
+        Book result = bookController.update(id, bookPatchDto);
+
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(updatedBook.getTitle(), result.getTitle())
+        );
+
+        verify(mockedBookService, times(1)).findById(id);
+        verify(mockedBookService, times(1)).updateBook(bookToUpdate, bookPatchDto);
+    }
+
+    @Test
+    void givenNotExistingBookId_WhenUpdate_ThenThrowResponseStatusException(){
+        Long id = 1L;
+        BookPatchDto bookPatchDto = new BookPatchDto();
+
+        when(mockedBookService.findById(id)).thenReturn(Optional.empty());
+
+        ResponseStatusException thrownException =
+                assertThrows(ResponseStatusException.class, () -> bookController.update(id, bookPatchDto));
+
+        assertAll(
+                () -> assertNotNull(thrownException),
+                () -> assertEquals(HttpStatus.NOT_FOUND, thrownException.getStatus()),
+                () -> assertEquals(
+                        thrownException.getStatus() + " \"" + String.format(BOOK_NOT_FOUND_ERROR_MESSAGE , id) +"\"",
+                        thrownException.getMessage())
+        );
+
+        verify(mockedBookService, times(1)).findById(id);
+    }
+
+    @Test
+    void givenBookId_WhenDelete_ThenCallDeleteMethod(){
+        Long id = 1L;
+        Book bookToDelete = new Book();
+
+        when(mockedBookService.findById(id)).thenReturn(Optional.of(bookToDelete));
+
+        bookController.delete(id);
+
+        verify(mockedBookService, times(1)).findById(id);
+        verify(mockedBookService, times(1)).delete(bookToDelete);
+    }
+
+    @Test
+    void givenInvalidBookId_WhenDelete_ThenThrowResponseStatusException(){
+        Long id = 1L;
+
+        when(mockedBookService.findById(id)).thenReturn(Optional.empty());
+
+        ResponseStatusException thrownException =
+                assertThrows(ResponseStatusException.class, () -> bookController.delete(id));
+
+        assertAll(
+                () -> assertNotNull(thrownException),
+                () -> assertEquals(HttpStatus.NOT_FOUND, thrownException.getStatus()),
+                () -> assertEquals(
+                        thrownException.getStatus() + " \"" + String.format(BOOK_NOT_FOUND_ERROR_MESSAGE , id) +"\"",
+                        thrownException.getMessage())
+        );
     }
 }
